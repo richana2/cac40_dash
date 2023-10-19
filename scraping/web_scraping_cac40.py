7a2bbc0678c5ddb6c3b87b3f06d4571693d73458
Nathan RICHARD <nathan.richard2@univ-rouen.fr>
2023-10-19 21:43:01 +0200

    Extract_maj

--- scraping/web_scraping_cac40.py
+++ scraping/web_scraping_cac40.py
@@ -1,46 +1,46 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import yfinance as yf
+from datetime import date
+import boto3
+import os
 
-class Cac40scraping:
-  def __init__(self,cac40Info, start, end):
-    """
-    Initializes an instance of the CAC40 web scraper.
 
-    Args:
-    - cac40Info: a dictionary containing information about the CAC40 index
-    - start: a string representing the start date of the data to be scraped
-    - end: a string representing the end date of the data to be scraped
-    """
-    self.info = cac40Info
-    self.start = start
-    self.end = end
+def cac40scraping(start, end):
+    try:
+      cac40Info = pd.read_html('https://en.wikipedia.org/wiki/CAC_40')[4]
+      d = yf.download(
+        list(cac40Info.Ticker),
+        start = start,
+        end = end
+      )
+      return d
+    except Exception as e:
+      print(e)
+      print('Failed to download the data from yfinance.')
+      return None
 
-  def dlData(self):
-    """
-    Downloads the data for the specified time period and returns it as a pandas DataFrame.
-
-    Returns
-    -------
-    pandas.DataFrame
-        A DataFrame containing the stock data for the specified time period.
-    """
-    d = yf.download(
-      list(cac40Info.Ticker),
-      start = self.start,
-      end = self.end
-    )
-    return d
-
+def save_to_S3(filename, date, bucket):
+  try:
+    s3 = boto3.client('s3')
+    
+    s3.upload_file(filename, bucket, date + '/' + filename)
+  except Exception as e:
+    print(e)
+    print('Failed to upload the data to S3.')
+    
 
 if __name__ == '__main__':
+  #################################Scraping####################################
   start = '2020-01-01'
-  end = '2023-10-13'
+  end =  date.today().strftime("%Y-%m-%d")
+  filename = 'CAC40_raw_data_' + start + '_to_' + end +'.csv'
   
-  cac40Info = pd.read_html('https://en.wikipedia.org/wiki/CAC_40')[4]
-  cac40Info.head()
-
-  Scraper = Cac40scraping(cac40Info, start, end)
-  data = Scraper.dlData()
-
-  data.to_csv('CAC40_raw_data_' + start + '_to_' + end +'.csv')
+  data = cac40scraping(start, end)
+  data.to_csv(filename)
+  #############################################################################
+  
+  #################################Upload to S3################################
+  save_to_S3(filename, end, 'cac40-raw-data')
+  #############################################################################
+  
